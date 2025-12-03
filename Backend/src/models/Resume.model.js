2f2a5fedd9cc1eb929b6d4e1780a7031d5efaa84
Aayush240305 <pawarpravin99163@gmail.com>
2025-12-03 19:41:51 +0530

    resume model added

---
+++ Backend/src/models/Resume.model.js
@@ -0,0 +1,62 @@
+import mongoose, { Schema } from "mongoose";
+
+const resumeSchema = new Schema(
+  {
+    name: { type: String }, 
+    
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+
+    color: { type: String },
+    template: { type: String },
+
+    personalInfo: {
+      photo: { type: String },
+      name: { type: String },
+      phoneNo: { type: String },
+      location: { type: String },
+      email: { type: String },
+      website: { type: String },
+      linkedin: { type: String },
+      profession: { type: String },
+    },
+
+    professionalSummary: { type: String },
+
+    experience: [
+      {
+        company: { type: String },
+        title: { type: String },
+        start: { type: String },
+        end: { type: String },
+        description: { type: String },
+      },
+    ],
+
+    education: [
+      {
+        institute: { type: String },
+        degree: { type: String },
+        completitionYear: { type: String },
+        cgpa: { type: String },
+      },
+    ],
+
+    project: [
+      {
+        name: { type: String },
+        tech: { type: String },
+        description: { type: String },
+      },
+    ],
+
+    skills: [{ type: String }],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Resume = mongoose.model("Resume", resumeSchema);
